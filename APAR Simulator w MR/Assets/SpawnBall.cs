323bac20638791a62312fe7791f9c67c3f1023ee
dimas235 <dimasprayuda80@gmail.com>
2024-02-17 14:00:28 +0700

    bounce grab
    menambahkan script untuk mengambil bola merah

--- APAR Simulator w MR/Assets/SpawnBall.cs
+++ APAR Simulator w MR/Assets/SpawnBall.cs
@@ -2,25 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnBall : MonoBehaviour
+public class SpawnAndGrabBall : MonoBehaviour
 {
     public GameObject prefab;
     public float spawnSpeed = 5f;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    private GameObject currentBall;
+    private bool isGrabbing = false;
 
     // Update is called once per frame
     void Update()
     {
-        if(OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger))
+        // Spawn a ball when the index trigger is pressed
+        if(OVRInput.GetDown(OVRInput.Button.SecondaryIndexTrigger) && !isGrabbing)
         {
-            GameObject spawnedBall = Instantiate(prefab, transform.position, Quaternion.identity);
-            Rigidbody spawnedBallRB = spawnedBall.GetComponent<Rigidbody>();
-            spawnedBallRB.velocity = transform.forward * spawnSpeed;
+            currentBall = Instantiate(prefab, transform.position, Quaternion.identity);
+            Rigidbody rb = currentBall.GetComponent<Rigidbody>();
+            rb.velocity = transform.forward * spawnSpeed;
+        }
+
+        // Grab the ball when the hand trigger is pressed
+        if(OVRInput.GetDown(OVRInput.Button.SecondaryHandTrigger))
+        {
+            if (currentBall != null)
+            {
+                currentBall.GetComponent<Rigidbody>().isKinematic = true;
+                currentBall.transform.SetParent(transform);
+                isGrabbing = true;
+            }
+        }
+
+        // Release the ball when the hand trigger is released
+        if(OVRInput.GetUp(OVRInput.Button.SecondaryHandTrigger))
+        {
+            if (currentBall != null)
+            {
+                currentBall.GetComponent<Rigidbody>().isKinematic = false;
+                currentBall.transform.SetParent(null);
+                currentBall = null;
+                isGrabbing = false;
+            }
         }
     }
 }
